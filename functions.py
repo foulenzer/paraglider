ed2c51333164c7084400e50e41e68203088ccbad
Benjamin Intfeld <github@foulenzer.me>
2020-08-13 21:46:43 +0200

    v0.1

---
+++ functions.py
@@ -0,0 +1,55 @@
+# functions.py
+
+# import stuff
+import warnings
+import requests
+import re
+
+# ignore openSSL warnings
+warnings.filterwarnings("ignore")
+
+# function request_body()
+# getting the body data of an http request
+# parameter:
+#	- type    -> request type (GET or POST)
+#	- subject -> subject to run scan on
+#	- data    -> json data for POST requests
+def requestContent(reqtype, subject, data):
+	if reqtype == 'GET':
+		try:
+			req = requests.get(subject, timeout=5)
+		except:
+			print('Sir, could not send request. Is host up?')
+			return 'FALSE'
+		else:
+			return req.content
+	elif reqtype == 'POST':
+		try:
+			req = requests.post(subject, data = {data}, timeout=5)
+		except:
+			print('Sir, could not send request. Status code: ' + req.status_code)
+			return 'FALSE'
+		else:
+			return req.content
+	else:
+		print('Sir, something went wrong with the type parameter.')
+		return 'FALSE'
+
+# function parseContent()
+# find pattern in source code of http request
+# parameter:
+#	- reqcontent -> source code as string
+def parseContent (reqcontent):
+	regex_list = [
+		'<input',
+		'action=',
+	]
+	generic_regex = re.compile( '|'.join( regex_list) )
+
+	for line in reqcontent.split("\n"):
+		if len(line) > 120:
+			continue
+		match = re.search(generic_regex, line)
+		if match:
+			print('[Found possible parameter]' + match.string.strip())
+
---
+++ paraglider.py
@@ -0,0 +1,39 @@
+# paraglider.py
+# by foulenzer
+
+# import stuff
+import warnings
+import sys
+import re
+from functions import requestContent, parseContent
+
+# ignore openSSL warnings
+warnings.filterwarnings("ignore")
+
+# prepare stuff
+count = 0
+body  = ''
+
+# Looks beatiful, no?
+print('### paraglider.py - parameter discovery ###')
+print('# Checking subject_list #')
+
+# get subject
+try:
+	subject_list = open(sys.argv[1], 'r').readlines()
+except:
+	print('Sir, something is wrong with the subject argument. x(')
+else:
+	for subject in subject_list:
+		print(subject.strip())
+		try:
+			content = requestContent('GET', 'https://' + subject.strip(), '')
+		except:
+			try:
+				content = requestContent('GET', 'http://' + subject.strip(), '')
+			except:
+				print('Sir, something is wrong with the domain parameter.')
+			else:
+				parseContent(content)
+		else:
+			parseContent(content)
